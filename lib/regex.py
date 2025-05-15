962eca54a6308ac0aba85dc20fd24a6f0dea7beb
Frankincense <frankincensewelsey@gmail.com>
2025-05-15 15:09:55 +0300

    regular expressions

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
+# NOTE: There are only a few tests included, so multiple solutions will work. 
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[A-Z][a-z]{3}\s[A-Z][a-z]{3}|\w{4}\s\w{6}-\w{3}|\w'\w{6}"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[0-5]{10}|[0-5]{3}-[0-5]{3}-[0-5]{4}|\([0-5]{3}\)\s[0-5]{3}-[0-5]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[a-z.]+[a-z0-9]+@\w+\.[a-z]{3}"
 email_regex = re.compile(email_address)
